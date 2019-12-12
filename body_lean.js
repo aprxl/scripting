2d0002f44f291bb41592654ae9c9f85595159922
aprxl <56981371+aprxl@users.noreply.github.com>
2019-12-11 21:30:04 -0300

    Jitter update

--- body_lean.js
+++ body_lean.js
@@ -11,30 +11,49 @@
 // Our main variables
 var main = {
     condition: 0,
-    last_condition: -1
+    last_condition: -1,
+
+    jittering: false
 };
 
 //endregion
 
 //region menu
 
+// :eyes:
+//const indicator = UI.AddCheckbox("Desync length indicator");
+
 // Creates our condition selector
 const current_condition = UI.AddDropdown("Condition", ["Standing", "Moving", "Slow-walking", "Jumping"]);
 
 // Create the rest of the elements
 const normal = {
-    0: UI.AddSliderInt("Standing body lean", 0, 100),
-    1: UI.AddSliderInt("Moving body lean", 0, 100),
-    2: UI.AddSliderInt("Slow-walking body lean", 0, 100),
-    3: UI.AddSliderInt("Jumping body lean", 0, 100)
-}
+    0: UI.AddSliderInt("Standing body lean", 0, 150),
+    1: UI.AddSliderInt("Moving body lean", 0, 150),
+    2: UI.AddSliderInt("Slow-walking body lean", 0, 150),
+    3: UI.AddSliderInt("Jumping body lean", 0, 150)
+};
 
 const inverted = {
-    0: UI.AddSliderInt("Standing inverted body lean", 0, 100),
-    1: UI.AddSliderInt("Moving inverted body lean", 0, 100),
-    2: UI.AddSliderInt("Slow-walking inverted body lean", 0, 100),
-    3: UI.AddSliderInt("Jumping inverted body lean", 0, 100)
-}
+    0: UI.AddSliderInt("Standing inverted body lean", 0, 150),
+    1: UI.AddSliderInt("Moving inverted body lean", 0, 150),
+    2: UI.AddSliderInt("Slow-walking inverted body lean", 0, 150),
+    3: UI.AddSliderInt("Jumping inverted body lean", 0, 150)
+};
+
+const jitter_type = {
+    0: UI.AddDropdown("Standing jitter mode", ["Offset", "Circular", "Random"]),
+    1: UI.AddDropdown("Moving jitter mode", ["Offset", "Circular", "Random"]),
+    2: UI.AddDropdown("Slow-walking jitter mode", ["Offset", "Circular", "Random"]),
+    3: UI.AddDropdown("Jumping jitter mode", ["Offset", "Circular", "Random"])
+};
+
+const jitter_ranges = {
+    0: UI.AddSliderInt("Standing jitter range", 0, 58),
+    1: UI.AddSliderInt("Moving jitter range", 0, 58),
+    2: UI.AddSliderInt("Slow-walking jitter range", 0, 58),
+    3: UI.AddSliderInt("Jumping jitter range", 0, 58)
+};
 
 // Group our condition's names to make our lives easier
 const conditions = {
@@ -42,7 +61,7 @@ const conditions = {
     1: "Moving",
     2: "Slow-walking",
     3: "Jumping"
-}
+};
 
 //endregion
 
@@ -57,11 +76,11 @@ const conditions = {
  * @returns {number}
  */
 const velocity = function(player) {
-    const vel = Entity.GetProp(player, "CBasePlayer", "m_vecVelocity[0]")
+    const vel = Entity.GetProp(player, "CBasePlayer", "m_vecVelocity[0]");
     return (
         Math.sqrt(vel[0] * vel[0] + vel[1] * vel[1])
     )
-}
+};
 
 /**
  * Checks if a player entity is jumping
@@ -74,7 +93,7 @@ const is_jumping = function(player) {
         // s/o @leed
         Entity.GetProp(player, "CBasePlayer", "m_hGroundEntity")
     )
-}
+};
 
 //endregion
 
@@ -99,11 +118,15 @@ function update_visibility()
         // Get our properties
         const normal_label = conditions[i] + " body lean";
         const inverted_label = conditions[i] + " inverted body lean";
+        const jitter_cond_label = conditions[i] + " jitter mode";
+        const jitter_val_label = conditions[i] + " jitter range";
         const enabled = i === _cond;
 
         // Update menu elements
         UI.SetEnabled("Misc", "JAVASCRIPT", "Script items", normal_label, enabled);
         UI.SetEnabled("Misc", "JAVASCRIPT", "Script items", inverted_label, enabled);
+        UI.SetEnabled("Misc", "JAVASCRIPT", "Script items", jitter_cond_label, enabled);
+        UI.SetEnabled("Misc", "JAVASCRIPT", "Script items", jitter_val_label, enabled);
     }
 }
 
@@ -115,32 +138,76 @@ update_visibility();
  */
 function update_condition()
 {
+    // Gets the local player
     const player = Entity.GetLocalPlayer();
 
+    // If our local player is invalid or isn't alive, then reset.
     if (!player || !Entity.IsAlive(player))
     {
         main.condition = 0;
-        return
+        return;
     }
 
+    // Get slow-walk state
     const slowwalk = UI.IsHotkeyActive("Anti-Aim", "Extra", "Slow walk");
 
-    if (is_jumping(player)) {
-        main.condition = 3;
-        return;
+    // If it's jumping, then return "Jumping"
+    if (is_jumping(player)) {main.condition = 3; return;} else {
+
+        // Otherwise, check for player's velocity
+        if (velocity(player) > 2) {
+            // If we're moving and pressing the slow-walk key, then return "Slow-walking"
+            if (slowwalk) {
+                main.condition = 2;
+                return;
+            }
+
+            //Otherwise, return "Moving"
+            main.condition = 1;
+            return;
+        }
+
     }
 
-    if (slowwalk) {
-        main.condition = 2;
-        return;
-    }
-
-    if (velocity(player) > 2) {
-        main.condition = 1;
-        return;
-    }
-
+    // If we're on the ground and not moving, then return "Standing"
     main.condition = 0;
+}
+
+function get_jitter_values()
+{
+    // Gets the local player
+    const player = Entity.GetLocalPlayer();
+
+    // If our local player is invalid or isn't alive, then return 0.
+    if (!player || !Entity.IsAlive(player))
+    {
+        return 0;
+    }
+
+    // Get our current jitter info
+    const _mode = UI.GetValue("Misc", "JAVASCRIPT", "Script items", conditions[main.condition] + " jitter mode");
+    const _range = UI.GetValue("Misc", "JAVASCRIPT", "Script items", conditions[main.condition] + " jitter range");
+
+    // If our range is 0, then no need to jitter.
+    if (_range === 0) {
+        return 0;
+    }
+
+    // Offset
+    if (_mode === 0) {
+        main.jittering = !main.jittering;
+        return main.jittering ? _range : 0;
+    }
+
+    // Circular
+    if (_mode === 1) {
+        return (Globals.Tickcount() / 2) % _range;
+    }
+
+    // Random
+    if (_mode === 2) {
+        return Math.random() * _range;
+    }
 }
 
 /**
@@ -160,10 +227,11 @@ function do_anti_aim()
 
     // Switch between inverted and normal lean
     const yaw = UI.IsHotkeyActive("Anti-Aim", "Fake angles", "Inverter") ? leans.inv : leans.normal;
+    const jitter_offset = get_jitter_values();
 
     // Update our anti-aim
     UI.SetValue("Anti-Aim", "Rage Anti-Aim", "Yaw offset",
-        59 - (yaw * 0.59)
+        59 - (yaw * 0.59) + jitter_offset
     );
 }
 
